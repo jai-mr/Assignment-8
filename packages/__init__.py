47622526b4b6f46e7fff26f75c517b268e9cf8d5
jai-mr <68414965+jai-mr@users.noreply.github.com>
2020-09-18 18:14:13 +0530

    Add files via upload

---
+++ packages/__init__.py
@@ -0,0 +1,7 @@
+from .augmentation import CIFAR10_Transforms 
+from .data import  CIFAR10DataLoader
+from .Net import *
+from .test import Test
+from .train import Train
+from .plot import mis, graph, testvtrain, class_acc
+from .summary import model_summary, display
